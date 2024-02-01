64da8754e4499d0335cbabfbfc408899aee30033
“c.b.olha” <c.b.olha@viseven.com>
2024-02-01 20:58:25 -0300

    added navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import "../styles/Navbar.css";
+import { BiWorld } from "react-icons/bi";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <BiWorld className="navbar-icon" />
+      <h1 className="app-title">my travel journal.</h1>
+    </nav>
+  );
 };
 
 export default Navbar;
