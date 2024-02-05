f65a8acdc5c5fc9c339ff133f76c7de421114c06
“c.b.olha” <c.b.olha@viseven.com>
2024-02-05 10:54:38 -0300

    added card info based on the data array

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,15 @@
 import "../styles/MainContent.css";
 import Card from "./Card";
+import data from "../data.js";
+
+console.log(data);
 
 const MainContent = () => {
   return (
     <main className="main-content">
-      <Card />
+      {data.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
     </main>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://maps.app.goo.gl/dogYYRDqxk7QyHjH6",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century s most famous and distinctive buildings.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1549180030-48bf079fb38a?q=80&w=2971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1683713552787-eb7843b46df8?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://maps.app.goo.gl/u6wVCs7oHd38SwSj7",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1677343210638-5d3ce6ddbf85?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
